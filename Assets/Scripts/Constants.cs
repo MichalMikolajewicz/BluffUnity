3121bb0ab47473a87d32bdc0ed30f405d8c7e162
Michał Mikołajewicz <michal.mikolajewicz@gmail.com>
2022-11-29 21:28:26 +0100

    add simple card renderer

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -4,10 +4,10 @@ using System.ComponentModel;
 public enum Suits
 {
     NoSuits = -1,
-    Spades = 0,
-    Clubs = 1,
-    Diamonds = 2,
-    Hearts = 3,
+    Spade = 0,
+    Club = 1,
+    Diamond = 2,
+    Heart = 3,
 }
 
 
@@ -15,31 +15,17 @@ public enum Ranks
 {
     [Description("No Ranks")]
     NoRanks = -1,
-    [Description("2")]
-    Two = 2,
-    [Description("3")]
-    Three = 3,
-    [Description("4")]
-    Four = 4,
-    [Description("5")]
-    Five = 5,
-    [Description("6")]
-    Six = 6,
-    [Description("7")]
-    Seven = 7,
-    [Description("8")]
-    Eight = 8,
-    [Description("9")]
+    [Description("09")]
     Nine = 9,
     [Description("10")]
     Ten = 10,
-    [Description("J")]
+    [Description("11")]
     Jack = 11,
-    [Description("Q")]
+    [Description("12")]
     Queen = 12,
-    [Description("K")]
+    [Description("13")]
     King = 13,
-    [Description("A")]
+    [Description("14")]
     Ace = 14,
 }
 
--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -4,4 +4,15 @@ using UnityEngine;
 
 public class Game : MonoBehaviour
 {
+    public GameObject CardPrefab;
+
+
+    void Start()
+    {
+        Instantiate(CardPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+
+    void Update()
+    {
+    }
 }
